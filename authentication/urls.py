dd9aa88d5219e6760f1ca0063225291110897180
Bijay Khadka <bk006822@gmail.com>
2023-08-16 20:53:01 +0545

    added: token auth user level

--- authentication/urls.py
+++ authentication/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [ 
     path('user/registration', views.UserRegistrationView.as_view(), name='user_registration'),               
-
+    path('user/login', views.UserLoginView.as_view(), name='user_login'),
 ]
